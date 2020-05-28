a5aef2cd0a12dbb759bd96f62c6221cf8acd5656
manolishah <50911478+manolishah@users.noreply.github.com>
2020-05-29 00:37:46 +0530

    update

--- src/component/streams/StreamShow.js
+++ src/component/streams/StreamShow.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
+import flv from 'flv.js';
 class StreamShow extends React.Component{
+  constructor(props){
+    super(props);
+    this.VideoRef = React.createRef();
+  }
   componentDidMount(){
-    this.props.fetchStream(this.props.match.params.id);
+    const {id} = this.props.match.params;
+    this.props.fetchStream(id);
+   this.buildPlay();
+  }
+  componentDidUpdate(){
+    this.buildPlay();
+  }
+  componentWillUnmount(){
+    this.player.destroy();
+  }
+  buildPlay(){
+    if(this.player || !this.props.streams){
+      return;
+    }
+    const {id} = this.props.match.params;
+    this.player=flv.createPlayer({
+      type:'flv', //type of video is geat
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.VideoRef.current);
+    this.player.load();
+
   }
   render(){
     if(!this.props.streams){
@@ -11,6 +37,7 @@ class StreamShow extends React.Component{
     }
     return(
       <div>
+        <video ref={this.VideoRef} style={{width:'100%' }} controls />
         <h1>{this.props.streams.title}</h1>
         <h5>{this.props.streams.description}</h5>
       </div>
